ea1de99773a1bbd685dc8cb1839590f77d7fe4ef
jimakosk <jkon2@DESKTOP-FEKS7V2>
2023-09-17 10:44:07 +0300

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApi.Servises;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace ProductApi.Controllers
+{   
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {        
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _productService.GetProductsAsync();
+            if (products == null)
+                return NotFound();
+            
+            return Ok(products);
+        }
+    }
+
+}
---
+++ Servises/ProductService.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ProductApi.Models;
+namespace ProductApi.Servises
+{
+
+    public interface IProductService
+    {
+        Task<List<Product>> GetProductsAsync();
+    }
+
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            var response = await _httpClient.GetAsync("https://fakestoreapi.com/products");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<Product>>(content);
+            }
+            return null; 
+        }
+    }
+
+}
